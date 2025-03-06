4a782825eca35c0a0a239177fe469a8c768b3269
ronaldhernandez21 <rfhernandez@izertis.com>
2025-03-06 12:46:08 +0100

    primer commit

--- src/app/components/partials/tabs/tabs-routing.module.ts
+++ src/app/components/partials/tabs/tabs-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'home',
+        loadChildren: () => import('../../pages/home/home.module').then(m => m.HomePageModule)
       },
       {
         path: 'tab2',
